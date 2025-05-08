60471dfc7e0ca9c24cfac14885fedf242b16ee20
kaatrasa <valtteri.kaatrasalo@gmail.com>
2025-05-08 16:03:34 +0300

    Minimal version with Geoid support

--- data-installer/gravity/make-installers.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/sh
-for m in egm84 egm96 egm2008 wgs84; do
-  tar cfCjv ../../data-distrib/gravity-distrib/$m.tar.bz2 .. gravity/$m.egm{,.cof}
-  (
-      cd ..
-      rm -f ../data-distrib/gravity-distrib/$m.zip
-      zip ../data-distrib/gravity-distrib/$m.zip gravity/$m.egm{,.cof}
-  )
-done
